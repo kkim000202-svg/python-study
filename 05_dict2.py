a316caec1ae4731adb20d4215df1fdd8d61d2b89
kkim000202-svg <kkim000202@gmail.com>
2026-07-10 19:05:42 +0900

    Create 05_dict2.py

---
+++ 05_dict2.py
@@ -0,0 +1,17 @@
+fruit_shop = {
+    "apple": {
+        "price": 1500,
+        "stock": 20
+    },
+    "banana": {
+        "price": 800,
+        "stock": 30
+    }
+}
+
+# 할 일:
+# 1) apple의 price를 출력해봐
+# 2) banana의 stock을 출력해봐
+
+print(fruit_shop["apple"]["price"]) #fruit_shop → apple의 딕셔너리 → 그 안에서 price 값
+print(fruit_shop["banana"]["stock"])
