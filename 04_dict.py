7a1c532dd711e78bf111c97b5bfbc5f7227f7367
kkim000202-svg <kkim000202@gmail.com>
2026-07-10 18:54:16 +0900

    Create 04_dict.py

---
+++ 04_dict.py
@@ -0,0 +1,20 @@
+# 딕셔너리(dict)개념과 예시
+# model_info = {"name": "LSTM", "layers": 4}
+# model_info["name"]     # "LSTM"
+# model_info["layers"]   # 4
+
+# 아래 정보를 딕셔너리로 만들어봐:
+# - key "fruit" → "사과"
+# - key "price" → 1500
+# - key "stock" → 20   (재고 개수)
+
+# 만든 다음:
+# print(item["fruit"])
+# print(item["price"])
+# print(item["stock"])
+# 로 각각 출력해봐.
+
+item = {"fruit": "사과","price":1500,"stock":20}
+print(item["fruit"])
+print(item["price"])
+print(item["stock"])
